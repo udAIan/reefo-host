23f6fff528af624c6fc0eb72eac3c916aced1c7e
Udayan Maurya <udayanmaurya@gmail.com>
2025-09-30 17:55:53 -0700

    scrolling area

--- apps/client/src/routes/~app/~_layout/~chat.tsx
+++ apps/client/src/routes/~app/~_layout/~chat.tsx
@@ -57,9 +57,9 @@ const ChatComponent = () => {
   };
 
   return (
-    <div className="flex h-full flex-col">
+    <div className="overflow-y-auto flex flex-col h-full">
       {/* Messages container */}
-      <div className="flex-1 overflow-y-auto p-4">
+      <div className="flex-1 min-h-0 overflow-y-auto p-4">
         <div className="mx-auto flex max-w-3xl flex-col gap-4">
           {messages.map((message, index) => (
             <div
@@ -102,7 +102,7 @@ const ChatComponent = () => {
       </div>
 
       {/* Input area */}
-      <div className="border-t p-4">
+      <div className="border-t p-4 shrink-0">
         <div className="mx-auto flex max-w-3xl gap-2">
           <Input
             value={input}
